74a149c2232e726074069c383369e395d7e6812f
brozenberg <benrozenberg258@gmail.com>
2024-01-27 21:11:27 +0200

    init commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ page2Button.addEventListener("click", () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      navigator.serviceWorker.register('https://rozenben.github.io/pwa_test/service-worker.js').then(function(registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
         console.log('ServiceWorker registration failed: ', err);
