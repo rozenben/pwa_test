ac82bf0a999e288c3202700a7f12daea06fd0b0e
brozenberg <benrozenberg258@gmail.com>
2024-01-27 20:42:43 +0200

    try to add  pwa functions

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ page1Button.addEventListener("click", () => {
 page2Button.addEventListener("click", () => {
     window.location.href = "page2.html";
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    showInstallPromotion();
+  });
+  
