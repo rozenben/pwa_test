933618402754c82df244d2af6730f0db7e9bd6ac
brozenberg <benrozenberg258@gmail.com>
2024-01-27 20:06:36 +0200

    create a new files

---
+++ script.js
@@ -0,0 +1,10 @@
+const page1Button = document.getElementById("page1");
+const page2Button = document.getElementById("page2");
+
+page1Button.addEventListener("click", () => {
+    window.location.href = "page1.html";
+});
+
+page2Button.addEventListener("click", () => {
+    window.location.href = "page2.html";
+});
